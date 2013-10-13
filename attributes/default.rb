c95d5bb2a87649ecea2027c509219390361278b6
Conor Schaefer <conor.schaefer@gmail.com>
2013-10-13 12:23:56 -0400

    updates default node verison to 0.10.20

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,12 +24,12 @@ case node['platform_family']
     default['nodejs']['install_method'] = 'source'
 end
 
-default['nodejs']['version'] = '0.10.15'
-default['nodejs']['checksum'] = '87345ab3b96aa02c5250d7b5ae1d80e620e8ae2a7f509f7fa18c4aaa340953e8'
-default['nodejs']['checksum_linux_x64'] = '0b5191748a91b1c49947fef6b143f3e5e5633c9381a31aaa467e7c80efafb6e9'
-default['nodejs']['checksum_linux_x86'] = '7ff9fb6aa19a5269a5a2f7a770040b8cd3c3b528a9c7c07da5da31c0d6dfde4d'
+default['nodejs']['version'] = '0.10.20'
+#default['nodejs']['checksum'] = '87345ab3b96aa02c5250d7b5ae1d80e620e8ae2a7f509f7fa18c4aaa340953e8'
+default['nodejs']['checksum_linux_x64'] = 'b7c0a79acddfaeda8af221acdd18640ef5c62e8a'
+default['nodejs']['checksum_linux_x86'] = '709cd1a646447036abe3f57ea6e33bb1d411c229'
 default['nodejs']['dir'] = '/usr/local'
-default['nodejs']['npm'] = '1.3.5'
+#default['nodejs']['npm'] = '1.3.5'
 default['nodejs']['src_url'] = "http://nodejs.org/dist"
 default['nodejs']['make_threads'] = node['cpu'] ? node['cpu']['total'].to_i : 2
 default['nodejs']['check_sha'] = true
